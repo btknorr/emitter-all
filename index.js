889901c2a729f7851404a1a5d5ff889dafde6707
Brian Knorr <btknorr@gmail.com>
2013-04-05 11:06:23 -0500

    refactor

--- index.js
+++ index.js
@@ -1,61 +1,66 @@
 var Emitter = require('emitter');
 var index = require('indexof');
 
-var aliasOn = Emitter.prototype.on;
-Emitter.prototype.on = function(event, fn){
-    if (typeof event === 'function') {
-        (this._allCallbacks = this._allCallbacks || [])
-            .push(event);
-        return this;
-    }
-    return aliasOn(event, fn);
-};
+module.exports = EmitterAll;
 
-var aliasEmit = Emitter.prototype.emit;
-Emitter.prototype.emit = function(event){
-    aliasEmit(event);
-    if (this._allCallbacks) {
-        var args = [].slice.call(arguments, 1);
-        args.unshift(event);
-        for (var i = 0, len = this._allCallbacks.length; i < len; ++i) {
-            this._allCallbacks[i].apply(this, args);
-        }
-    }
-};
-
-var aliasRemoveAllListeners = Emitter.prototype.removeAllListeners;
-Emitter.prototype.off =
-Emitter.prototype.removeListener =
-Emitter.prototype.removeAllListeners = function(event, fn){
-    aliasRemoveAllListeners(event, fn);
-    if (this._allCallbacks) {
-        // all
-        if (0 == arguments.length) {
-            this._allCallbacks = [];
+function EmitterAll(obj) {
+    var emitter = Emitter(obj);
+    var aliasOn = emitter.on;
+    emitter.on = function(event, fn){
+        if (typeof event === 'function') {
+            (this._allCallbacks = this._allCallbacks || [])
+                .push(event);
             return this;
         }
-        // remove specific handler
-        var i = index(this._allCallbacks, fn._off || fn);
-        if (~i) this._allCallbacks.splice(i, 1);
+        return aliasOn(event, fn);
+    };
+
+    var aliasEmit = emitter.emit;
+    emitter.emit = function(event){
+        aliasEmit(event);
+        if (this._allCallbacks) {
+            var args = [].slice.call(arguments, 1);
+            args.unshift(event);
+            for (var i = 0, len = this._allCallbacks.length; i < len; ++i) {
+                this._allCallbacks[i].apply(this, args);
+            }
+        }
+    };
+
+    var aliasRemoveAllListeners = emitter.removeAllListeners;
+    emitter.off =
+    emitter.removeListener =
+    emitter.removeAllListeners = function(event, fn){
+        aliasRemoveAllListeners(event, fn);
+        if (this._allCallbacks) {
+            // all
+            if (0 == arguments.length) {
+                this._allCallbacks = [];
+                return this;
+            }
+            // remove specific handler
+            var i = index(this._allCallbacks, fn._off || fn);
+            if (~i) this._allCallbacks.splice(i, 1);
+            return this;
+        }
         return this;
-    }
-    return this;
-};
+    };
 
-var aliasListeners = Emitter.prototype.listeners;
-Emitter.prototype.listeners = function(event){
-    if (0 == arguments.length) {
-        return this._allCallbacks || [];
-    }
-    return aliasListeners(event);
-};
+    var aliasListeners = emitter.listeners;
+    emitter.listeners = function(event){
+        if (0 == arguments.length) {
+            return this._allCallbacks || [];
+        }
+        return aliasListeners(event);
+    };
 
-var aliasHasListeners = Emitter.prototype.hasListeners;
-Emitter.prototype.hasListeners = function(event){
-    if (0 == arguments.length) {
-        return !! this.listeners().length;
-    }
-    return aliasHasListeners();
-};
+    var aliasHasListeners = emitter.hasListeners;
+    emitter.hasListeners = function(event){
+        if (0 == arguments.length) {
+            return !! this.listeners().length;
+        }
+        return aliasHasListeners();
+    };
 
-module.exports = Emitter;
+    return emitter;
+}
