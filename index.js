8375f050d68c29f583536e8ebfc8f9eedff7f111
Brian Knorr <btknorr@gmail.com>
2013-04-05 11:11:54 -0500

    refactor

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function EmitterAll(obj) {
 
     var aliasEmit = emitter.emit;
     emitter.emit = function(event){
-        aliasEmit.apply(this, arguments);
+        aliasEmit(arguments);
         if (this._allCallbacks) {
             var args = [].slice.call(arguments, 1);
             args.unshift(event);
